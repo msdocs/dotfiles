a1fc778c08c1d3146b40d5664f4e742e2850fe1b
Mahmoud Saleh <msdocs@me.com>
2020-04-09 16:03:26 -0400

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,9 +7,11 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias less='less -R'
 alias g='git'
-
 alias rm='rm -i'                              # confirm removal
 alias cp='cp -i'                              # confirm copy
 alias mv='mv -i'                              # confirm move
 alias cal='gcal --starting-day=1'             # print simple calendar for current month
 alias weather='curl v2.wttr.in'               # print weather for current location (https://github.com/chubin/wttr.in)
+
+# clean terragrunt cache
+alias tg-clean='find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;'   
